636ce4a115694f423ba59aa9850e934293547a09
Francis Lee <francis@u.nus.edu>
2020-05-17 14:39:22 +0800

    Add trace task

--- alpha/alpha.ino
+++ alpha/alpha.ino
@@ -28,6 +28,14 @@ void setup() {
   }
 
   OT_ProtocolV2.begin();
+
+  xTaskCreate(
+    traceTask, /* Task function. */
+    "Trace", /* name of task. */
+    10000, /* Stack size of task */
+    NULL, /* parameter of the task */
+    2, /* priority of the task */
+    NULL); /* Task handle to keep track of created task */
 }
 
 int skips = 0;
@@ -45,45 +53,53 @@ void loop() {
     TS_HAL.lcd_printf("Date: %04d-%02d-%02d\n",     datetime.year, datetime.month, datetime.day);
     TS_HAL.lcd_printf("Time: %02d : %02d : %02d\n", datetime.hour, datetime.minute, datetime.second);
   }
+}
 
-  // blink once a second
-  TS_HAL.setLed(TS_Led::Red, true);
-  TS_HAL.sleep(TS_SleepMode::Default, 1);
-  TS_HAL.setLed(TS_Led::Red, false);
-
-  // don't turn off radio if we have connected clients
-  uint16_t connectedCount = OT_ProtocolV2.get_connected_count();
-  uint16_t sleepDuration = TS_HAL.random_get(1000, 3000);
-
-  Serial.print(F("Devices connected: "));
-  Serial.println(connectedCount);
-  if(connectedCount > 0) {
-    TS_HAL.sleep(TS_SleepMode::Task, sleepDuration);
-  } else {
-    TS_HAL.sleep(TS_SleepMode::Light, sleepDuration);
-  }
-
-  if(skips >= 5) { // vary the interval between scans here
-    skips = 0;
-    
-    // spend up to 1s scanning, lowest acceptable rssi: -95
-    OT_ProtocolV2.scan_and_connect(1, -95);
-  }
-  else
+void traceTask(void* parameter)
+{
+  uint16_t connectedCount;
+  uint16_t sleepDuration;
+  for(;;)
   {
-    ++skips;
+    // blink once a second
+    TS_HAL.setLed(TS_Led::Red, true);
+    TS_HAL.sleep(TS_SleepMode::Default, 1);
+    TS_HAL.setLed(TS_Led::Red, false);
+  
+    // don't turn off radio if we have connected clients
+    uint16_t connectedCount = OT_ProtocolV2.get_connected_count();
+    uint16_t sleepDuration = TS_HAL.random_get(1000, 3000);
+  
+    Serial.print(F("Devices connected: "));
+    Serial.println(connectedCount);
+    if(connectedCount > 0) {
+      TS_HAL.sleep(TS_SleepMode::Task, sleepDuration);
+    } else {
+      TS_HAL.sleep(TS_SleepMode::Light, sleepDuration);
+    }
+  
+    if(skips >= 5) { // vary the interval between scans here
+      skips = 0;
+      
+      // spend up to 1s scanning, lowest acceptable rssi: -95
+      OT_ProtocolV2.scan_and_connect(1, -95);
+    }
+    else
+    {
+      ++skips;
+    }
+  
+    // enable advertising
+    OT_ProtocolV2.advertising_start();
+    // just advertise for 1s
+    TS_HAL.sleep(TS_SleepMode::Task, 1000);
+    // disable advertising, get back to sleep
+    OT_ProtocolV2.advertising_stop();
+  
+    // Give some time for comms after broadcasts
+    // TODO: by right should wait T time after last uncompleted handshake before going back to sleep
+    TS_HAL.sleep(TS_SleepMode::Task, 100);
+  
+    // TODO: call OT update_characteristic_cache at least once every 15 mins
   }
-
-  // enable advertising
-  OT_ProtocolV2.advertising_start();
-  // just advertise for 1s
-  TS_HAL.sleep(TS_SleepMode::Task, 1000);
-  // disable advertising, get back to sleep
-  OT_ProtocolV2.advertising_stop();
-
-  // Give some time for comms after broadcasts
-  // TODO: by right should wait T time after last uncompleted handshake before going back to sleep
-  TS_HAL.sleep(TS_SleepMode::Task, 100);
-
-  // TODO: call OT update_characteristic_cache at least once every 15 mins
 }
