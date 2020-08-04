1ca6dbdb19165a8a3fb4d27685dad918c1f99d2a
James Yong <yongkimleng@gmail.com>
2020-08-04 03:09:16 -0700

    move defaults to storage cpp/h

--- alpha/alpha.ino
+++ alpha/alpha.ino
@@ -13,65 +13,12 @@
 // - look at mods/boards.diff.txt -- set CPU to 80mhz instead of 240mhz
 //
 
-#ifndef DEFAULT_UID
-#define DEFAULT_UID "0123456789"
-#endif
-#ifndef WIFI_SSID
-#define WIFI_SSID "test"       // Enter your SSID here
-#endif
-#ifndef WIFI_PASS
-#define WIFI_PASS "password"    // Enter your WiFi password here
-#endif
-
-void set_default_settings()
-{ 
-  TS_Settings* settings = TS_Storage.settings_get();
-
-  bool settingsUpdated = false;
-
-  if (strcmp(settings->userId, "") == 0)
-  {
-    memset(settings->userId, '\0', sizeof(settings->userId));
-    strcpy(settings->userId, DEFAULT_UID);
-    settingsUpdated = true;
-    log_i("Setting default UID");
-  }
-
-  if (strcmp(settings->wifiSsid, "") == 0)
-  {
-    memset(settings->wifiSsid, '\0', sizeof(settings->wifiSsid));
-    strcpy(settings->wifiSsid, WIFI_SSID);
-    settingsUpdated = true;
-    log_i("Setting default WIFI_SSID");
-  }
-
-  if (strcmp(settings->wifiPass, "") == 0)
-  {
-    memset(settings->wifiPass, '\0', sizeof(settings->wifiPass));
-    strcpy(settings->wifiPass, WIFI_PASS);
-    settingsUpdated = true;
-    log_i("Setting default WIFI_PASS");
-  }
-
-  log_i("Stored settings:");
-  log_i("Version: %d", settings->settingsVersion);
-  log_i("UID: %.32s", settings->userId);
-  log_i("WIFI_SSID: %.32s", settings->wifiSsid);
-  log_i("WIFI_PASS: %.32s", settings->wifiPass);
-
-  if (settingsUpdated)
-  {
-    TS_Storage.settings_save();
-  }
-}
 
 void setup() {
   TS_HAL.begin();
 
   TS_Storage.begin();
   log_w("Storage free: %d, %d%", TS_Storage.freespace_get(), TS_Storage.freespace_get_pct());
-
-  set_default_settings();
 
   OT_ProtocolV2.begin();
 
